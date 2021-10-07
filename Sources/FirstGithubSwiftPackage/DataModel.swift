2dedacbe87716fb0423aac6e19e388816815744d
fxu <feng.xu@screeningeagle.com>
2021-10-07 10:53:28 +0800

    add data model

---
+++ Sources/FirstGithubSwiftPackage/DataModel.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Feng Xu on 7/10/21.
+//
+
+import Foundation
+
+public struct DataModel {
+    
+    public let id = UUID()
+    
+    public var name = "XYZ"
+    
+}
